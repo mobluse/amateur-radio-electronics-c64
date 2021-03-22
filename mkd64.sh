166b224189e51bfaf588b64f1e00c296d9a99ad5
Mikael Bonnier <mikael.bonnier@gmail.com>
2021-03-23 00:42:28 +0100

    Added rlc-networks

--- mkd64.sh
+++ mkd64.sh
@@ -5,7 +5,7 @@ ls *.bas | sed 's/\.bas//' | while read p
     do cat $p.bas | awk '{print tolower($0)}' | petcat -w2 -o $p.prg
 done
 c1541 -format amateur-radio_m6,00 d64 amateur-radio-electronics.d64
-printf "menu\ncoil-loaded-shortened-dipol-antenna\nresistors-in-series-or-parallel\ncapacitors-in-series-or-parallel\ninductors-in-series-or-parallel\nzener-diode-voltage-regulator-design\n" | while read p
+printf "menu\ncoil-loaded-shortened-dipol-antenna\nresistors-in-series-or-parallel\ncapacitors-in-series-or-parallel\ninductors-in-series-or-parallel\nzener-diode-voltage-regulator-design\nrlc-networks\n" | while read p
     do c1541 -attach amateur-radio-electronics.d64 -write $p.prg $p
 done
 rm *.prg
