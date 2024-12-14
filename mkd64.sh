3404424fb90c460e8eaa01754747f1b9d35cd16d
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-12-14 17:57:22 +0100

    Update mkd64.sh

--- mkd64.sh
+++ mkd64.sh
@@ -7,7 +7,7 @@ ls *.bas | sed 's/\.bas//' | while read p
     do cat $p.bas | tr 'A-Za-z' 'a-zA-Z' | petcat -w2 -o $p.prg
 done
 c1541 -format amateur-radio_m6,00 d64 amateur-radio-electronics.d64
-printf "menu\ncoil-loaded-shortened-dipol-antenna\nresistors-in-series-or-parallel\ncapacitors-in-series-or-parallel\ninductors-in-series-or-parallel\n\
-zener-diode-voltage-regulator-design\nrlc-networks\ndecibel-calc\n" | while read p
+printf "menu\ncoil-loaded-shortened-dipol-antenna\nvswr-calibrations\nsatellite-dish-antenna\nresistors-in-series-or-parallel\ncapacitors-in-series-or-parallel\n\
+inductors-in-series-or-parallel\nzener-diode-voltage-regulator-design\nrlc-networks\ndecibel-calc\n" | while read p
     do c1541 -attach amateur-radio-electronics.d64 -write $p.prg $p
 done
